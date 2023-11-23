5f8d0f216b976f7616d61f56f164a3d6484c7a78
diamondburned <diamond@arikawa-hi.me>
2023-11-23 13:42:43 -0800

    Run go generate

--- extension/extracss/updaters_generated.js
+++ extension/extracss/updaters_generated.js
@@ -2,12 +2,35 @@
 // deno-lint-ignore-file
 // This code was bundled using `deno bundle` and it's not recommended to edit it manually
 
+function html(strings, ...values) {
+    const parts = [
+        strings[0]
+    ];
+    for(let i = 0; i < values.length; i++){
+        parts.push(String(values[i]));
+        parts.push(strings[i + 1]);
+    }
+    return parts.join("");
+}
 const updates = [
     {
         selector: `[data-overlay="journal"] ul li`,
         update: (listItems)=>{
             listItems.forEach((li)=>{
-                li.innerHTML = li.textContent.replaceAll(/(Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|today|tomorrow|in \d* days|\d{2}:\d{2})/g, `<span class="journal-date">$1</span>`);
+                li.innerHTML = li.textContent.replaceAll(/(Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|today|tomorrow|in \d* days|\d{2}:\d{2})/g, (match)=>{
+                    const classes = [
+                        "journal-date"
+                    ];
+                    switch(match){
+                        case "today":
+                            classes.push("journal-today");
+                            break;
+                        case "tomorrow":
+                            classes.push("journal-tomorrow");
+                            break;
+                    }
+                    return html`<span class="${classes.join(" ")}">${match}</span>`;
+                });
             });
         }
     }
